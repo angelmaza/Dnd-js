8dcdfbaffc0fe3f035e51297a220a91f4fe30c7f
Angel <angel.mazarias.salgado@gmail.com>
2025-10-29 21:24:11 +0100

    Eliminar turboMode y ajustar configuración

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configuración básica, sin turboMode, ya no es necesario.
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // Evitar problemas con 'fs' en el cliente
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
